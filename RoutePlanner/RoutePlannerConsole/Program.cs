a4e97ff8859aeb0a5535d6ecdf5fd3d10514523a
Stefan Wohlgensinger <stefan.wohlgensinger@students.fhnw.ch>
2017-03-12 14:53:32 +0100

    lab01 - Aufgabe 01: added RoutePlannerconsoleApp.cs

--- RoutePlanner/RoutePlannerConsole/Program.cs
+++ RoutePlanner/RoutePlannerConsole/Program.cs
@@ -8,8 +8,6 @@ namespace RoutePlannerConsole
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-        }
+        
     }
 }
---
+++ RoutePlanner/RoutePlannerConsole/RoutePlannerConsoleApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Reflection;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerConsole
+{
+    class RoutePlannerConsoleApp
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My first C# Program: {0}", Assembly.GetExecutingAssembly().GetName().Version);
+            Console.WriteLine("Press any key to quit");
+            Console.ReadKey();
+        }
+    }
+}
