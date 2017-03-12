53e8987139f854ecbf2a8112f91cbffac06dd07f
Stefan Wohlgensinger <stefan.wohlgensinger@students.fhnw.ch>
2017-03-12 15:06:21 +0100

    lab01: finished.

--- RoutePlanner/RoutePlannerConsole/RoutePlannerConsoleApp.cs
+++ RoutePlanner/RoutePlannerConsole/RoutePlannerConsoleApp.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 using System.Reflection;
 
+/*Import WayPoint.cs */
+using Fhnw.Ecnf.RoutePlanner.RoutePlannerLib;
+
 namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerConsole
 {
     class RoutePlannerConsoleApp
@@ -14,6 +17,11 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerConsole
         static void Main(string[] args)
         {
             Console.WriteLine("My first C# Program: {0}", Assembly.GetExecutingAssembly().GetName().Version);
+
+            var wayPoint = new WayPoint("Windisch", 47.479319847061966, 8.212966918945312);
+            Console.WriteLine($"{wayPoint.Name}: {wayPoint.Latitude}/{wayPoint.Longitude}");
+
+            //Um zu verhindern, dass Konsolenfenster gleich wieder geschlossen wird:
             Console.WriteLine("Press any key to quit");
             Console.ReadKey();
         }
