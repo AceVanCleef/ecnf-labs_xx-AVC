9d78159f1a093b0cf441654c756c964554e10fb2
Stefan Wohlgensinger <stefan.wohlgensinger@students.fhnw.ch>
2017-04-07 18:15:17 +0200

    Aufgabe 02: copied everything from Yanick since I had no clue what to do :/

--- RoutePlanner/RoutePlannerLib/RouteRequestWatcher.cs
+++ RoutePlanner/RoutePlannerLib/RouteRequestWatcher.cs
@@ -15,6 +15,11 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
         /* City: angefragte Stadt. int: Anz. Anfragen für City. */
         private Dictionary<City, int> RequestStatistics = new Dictionary<City, int>();
 
+
+        /* lab06: tracks request dates */
+        private Dictionary<DateTime, List<City>> requestsDate = new Dictionary<DateTime, List<City>>();
+
+
         /// <summary>
         /// THE EVENT HANDLER: it must have the same signature as the event's delegate.
         /// </summary>
@@ -39,6 +44,16 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
                 RequestStatistics.Add(key, value + 1);
             }
 
+            /*lab 06: */
+            if (requestsDate.ContainsKey(GetCurrentDate))
+            {
+                requestsDate[GetCurrentDate].Add(e.ToCity);
+            }
+            else
+            {
+                requestsDate.Add(GetCurrentDate, new List<City>());
+                requestsDate[GetCurrentDate].Add(e.ToCity);
+            }
 
             //Zählerstände auf Konsole ausgeben
             Console.WriteLine($"ToCity: {key.Name} has been requested {(GetCityRequests(key))} times");
@@ -70,17 +85,26 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
 
         public IEnumerable<City> GetBiggestCityOnDay(DateTime day)
         {
-            return null;
+            //Was waren die drei...             -> .Take(3)
+            //bevölkerungsreichsten Städte,...  -> .OrderByDescending(city => city.Population)
+            //die an einem bestimmten Tag...    ->  .Where( ? )
+            //abgefragt wurden?
+            if (requestsDate.ContainsKey(day))
+            {
+                return requestsDate[day].Distinct().OrderByDescending(city => city.Population).Take(3);
+            }
+            return new List<City>();
+
         }
 
         public IEnumerable<City> GetLongestCityNamesWithinPeriod(DateTime from, DateTime to)
         {
-            return null;
+            return requestsDate.Where(elem => elem.Key >= from && elem.Key <= to).SelectMany(elem => elem.Value).Distinct().OrderByDescending(cities => cities.Name.Length).Take(3);
         }
 
         public IEnumerable<City> GetNotRequestedCities(Cities cities)
         {
-            return null;
+            return cities.CityList.Except(requestsDate.Where(elem => elem.Key >= GetCurrentDate.AddDays(-14)).SelectMany(elem => elem.Value).Distinct());
         }
 
         /*************************** END of lab06 *******************************/
